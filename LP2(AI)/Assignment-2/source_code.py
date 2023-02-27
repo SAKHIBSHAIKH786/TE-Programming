62d2ef03c3181aec37e6212df8dfbf150bdff1fa
Sahil Naik <81952895+Sahil-Naik@users.noreply.github.com>
2023-02-27 13:52:05 +0530

    Update source_code.py

--- LP2(AI)/Assignment-2/source_code.py
+++ LP2(AI)/Assignment-2/source_code.py
@@ -8,3 +8,33 @@ def astar(start_node, goal_node):
   
   while open_set:
     current = min(open_set, key=lambda node:f_score[node])
+    
+    if current == goal_node:
+      path = [current]
+      while current in parent:
+        current = parent[current]
+        path.append(current)
+        
+      path.reverse()
+      return path
+    
+    open_set.remove(current)
+    closed_set.add(current)
+    
+    for neighbor in current_neighbors():
+      if neighbor in closed_set:
+        continue
+        
+      tentative_g_score = g_score[current]+current.distance(neighbour)
+      
+      if neightbor not in open_set:
+        open_set.add(neighbor)
+        
+      elif tentative_g_score >= g_score[neighbor]:
+        continue
+        
+      parent[neighbor] = current
+      g_score[neighbor] = tentative_g_score
+      f_score[neighbor] = tentative_g_score + neighbor.heuristic(goal_node)
+      
+  return None
